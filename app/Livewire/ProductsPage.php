d05500e32113aeb2edbdebc969679afb9cdac275
GabrielaRicaurte <gabrielaricaurte0109@gmail.com>
2025-02-25 14:09:20 -0400

    commit a

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\WithPagination;
 
+#[Title('Products - DcodeMania')]
 class ProductsPage extends Component
 {
+
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+        return view('livewire.products-page', [
+            'products' => $productQuery->get(6)
+        ]);
     }
 }
