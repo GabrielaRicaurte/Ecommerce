5b629c43d28cfb425c743681ed263ebbcc0885d6
GabrielaRicaurte <gabrielaricaurte0109@gmail.com>
2025-02-26 16:45:02 -0400

    commit 15-21:57

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\Title;
@@ -10,14 +12,42 @@ use Livewire\WithPagination;
 #[Title('Products - DcodeMania')]
 class ProductsPage extends Component
 {
-
     use WithPagination;
+
+    public $selected_categories = [];
+    public $selected_brands = [];
+    public $featured;
+    public $on_sale;
+    public $price_range = 3000;
 
     public function render()
     {
         $productQuery = Product::query()->where('is_active', 1);
+
+        if(!empty($this->selected_categories)) {
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if(!empty($this->selected_brands)) {
+            $productQuery->whereIn('brand_id', $this->selected_brands);
+        }
+
+        if($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+
+        if($this->on_sale) {
+            $productQuery->where('on_sale', 1);
+        }
+
+        if($this->price_range) {
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+
         return view('livewire.products-page', [
-            'products' => $productQuery->get(6)
+            'products' => $productQuery->paginate(9),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         ]);
     }
 }
